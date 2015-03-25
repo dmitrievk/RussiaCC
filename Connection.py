60041d560fc4e7c2dfc2c9120ee52839a0694464
Yingtao Tian <alan.yt.tian@gmail.com>
2015-03-25 10:45:40 -0400

    modify Connection.py

--- Connection.py
+++ Connection.py
@@ -2,8 +2,25 @@
 
 
 class Connection:
+    """
+    Base class for a connection
+    It should not be used directly
+    """
+
     def __init__(self):
         pass
+
+    def connect(self):
+        raise NotImplemented
+
+    def close(self):
+        raise NotImplemented
+
+
+class DirectConnection(Connection):
+    """
+    Direct connection (without using any VPN)
+    """
 
     def connect(self):
         pass
@@ -11,3 +28,15 @@ class Connection:
     def close(self):
         pass
 
+
+class OpenVPNConnection(Connection):
+    def __init__(config_path):
+        super().__init__()
+        
+        pass
+
+    def connect(self):
+        pass
+
+    def close(self):
+        pass
